271fbff611dc5c2475e450eb27255159cbb87393
dachanh <ghostcodert7@gmail.com>
2019-09-03 14:10:38 +0700

    update style and content loss function

---
+++ notebooks/Maximum_Mean_Discrepancy.py
@@ -0,0 +1,13 @@
+import numpy as np 
+
+
+class MMD(object):
+    def __init__(self):
+
+    def linear_kernel(X,y):
+        return np.dot(X.T,y)
+    
+    def polynominal(X,y,c):
+        return np.pow(np.dot(X.T,y) +c),d)
+    
+    
